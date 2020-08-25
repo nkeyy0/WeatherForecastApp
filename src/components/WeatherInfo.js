aec768a6af165e5c3aee3285447042331ae97f51
Nikita <komkovnk7@gmail.com>
2020-08-25 17:38:00 +0300

    add weather indicators

--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -5,6 +5,11 @@ class WeatherInfo extends React.Component {
     return(
         <div>
         <h2>Weather forecast in {this.props.enterCity}:</h2>
+        <p>Temperature:</p>
+        <p>Atmosphere pressure:</p>
+        <p>Wind:</p>
+        <p>Air humidity:</p>
+        <p>Cloudiness:</p>
         </div>
         )}
 }
